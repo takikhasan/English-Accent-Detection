911d6143c4f506e42263dbb322c5cdfb80836529
Takik Hasan <hasantakik@gmail.com>
2020-10-18 17:16:07 +0600

    Update prepare_dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -75,4 +75,4 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, 
 
 
 if __name__ == "__main__":
-    save_mfcc(DATASET_PATH, JSON_PATH, segment_duration=5)  # segment_duration in seconds
+    save_mfcc(DATASET_PATH, JSON_PATH, segment_duration=3)  # segment_duration in seconds
